c27a66a8aa4b31ce3eb62b0775dadbf1f7c857f2
CiroboyBR <ciroccuva@hotmail.com>
2021-03-24 04:22:21 -0300

    Versao 1.2. Oclusão do botão e adição de cores ao texto no resultado

--- app/src/main/java/com/ciro/calculadora_imc_bmi/MainActivity.kt
+++ app/src/main/java/com/ciro/calculadora_imc_bmi/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doAfterTextChanged
 
 class MainActivity : AppCompatActivity() {
@@ -41,20 +42,34 @@ class MainActivity : AppCompatActivity() {
     private fun calcula(pesoEDT: EditText, alturaEDT: EditText, resultadoTXT: TextView){
         var imc = calculaIMC(pesoEDT.text.toString().toFloat(), alturaEDT.text.toString().toFloat())
 
-        if (imc < 17.0)
-            resultadoTXT.setText("Muito abaixo do peso!\nIMC: "+imc.toString()+"")
-        else if (imc >= 17.0 && imc < 18.5 )
-            resultadoTXT.setText("Abaixo do peso!\nIMC: "+imc.toString()+"")
-        else if (imc >= 18.5 && imc < 25.0 )
-            resultadoTXT.setText("Peso normal!\nIMC: "+imc.toString()+"")
-        else if (imc >= 25.0 && imc < 30.0 )
-            resultadoTXT.setText("Acima do peso!\nIMC: "+imc.toString()+"")
-        else if (imc >= 30.0 && imc < 35.0 )
-            resultadoTXT.setText("Obesidade grau I!\nIMC: "+imc.toString()+"")
-        else if (imc >= 35.0 && imc <= 40.0 )
-            resultadoTXT.setText("Obesidade grau II!\nIMC: "+imc.toString()+"")
-        else if (imc > 40.0 )
-            resultadoTXT.setText("Obesidade grau III!\nIMC: "+imc.toString()+"")
+        if (imc < 17.0) {
+            resultadoTXT.setTextColor(getResources().getColor(R.color.red))
+            resultadoTXT.setText("Muito abaixo do peso!\nIMC: " + imc.toString() + "")
+        }
+        else if (imc >= 17.0 && imc < 18.5 ) {
+            resultadoTXT.setTextColor(getResources().getColor(R.color.orange))
+            resultadoTXT.setText("Abaixo do peso!\nIMC: " + imc.toString() + "")
+        }
+        else if (imc >= 18.5 && imc < 25.0 ) {
+            resultadoTXT.setTextColor(getResources().getColor(R.color.green))
+            resultadoTXT.setText("Peso normal!\nIMC: " + imc.toString() + "")
+        }
+        else if (imc >= 25.0 && imc < 30.0 ) {
+            resultadoTXT.setTextColor(getResources().getColor(R.color.orange))
+            resultadoTXT.setText("Acima do peso!\nIMC: " + imc.toString() + "")
+        }
+        else if (imc >= 30.0 && imc < 35.0 ) {
+            resultadoTXT.setTextColor(getResources().getColor(R.color.orange))
+            resultadoTXT.setText("Obesidade Grau 1!\nIMC: " + imc.toString() + "")
+        }
+        else if (imc >= 35.0 && imc <= 40.0 ) {
+            resultadoTXT.setTextColor(getResources().getColor(R.color.red))
+            resultadoTXT.setText("Obesidade Grau 2!\nIMC: " + imc.toString() + "")
+        }
+        else if (imc > 40.0 ) {
+            resultadoTXT.setTextColor( getResources().getColor(R.color.red))
+            resultadoTXT.setText("Obesidade Grau 3!\nIMC: " + imc.toString() + "")
+        }
     }
 
     //calcula o IMC
